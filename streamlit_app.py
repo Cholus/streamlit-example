52d283c5249c9374316c336cc3c79d9b30e5dbc7
Cholus <163318771+Cholus@users.noreply.github.com>
2024-05-29 16:11:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,17 @@ data = pd.read_csv('IMDB-Movie-Data.csv')
 # Título de la aplicación
 st.title('Análisis de Películas')
 
-# Filtros
-year_filter = st.sidebar.multiselect('Seleccione el Año:', options=data['Year'].unique())
+# Filtro de género
 genre_filter = st.sidebar.multiselect('Seleccione el Género:', options=data['Genre'].unique())
 
+# Slider de año
+min_year, max_year = int(data['Year'].min()), int(data['Year'].max())
+year_range = st.sidebar.slider('Seleccione el Rango de Años:', min_value=min_year, max_value=max_year, value=(min_year, max_year))
+
 # Aplicar filtros
-filtered_data = data
-if year_filter:
-    filtered_data = filtered_data[filtered_data['Year'].isin(year_filter)]
+filtered_data = data[(data['Year'] >= year_range[0]) & (data['Year'] <= year_range[1])]
 if genre_filter:
-    filtered_data = filtered_data[filtered_data['Genre'].isin(genre_filter)]
+    filtered_data = filtered_data[filtered_data['Genre'].str.contains('|'.join(genre_filter))]
 
 # Gráfico
 fig, ax = plt.subplots()
